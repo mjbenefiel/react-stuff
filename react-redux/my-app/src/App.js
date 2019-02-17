e68808f02e330d9d1033be34ec2f1c4528604a12
mjbenefiel <michaeljbenefiel@gmail.com>
2019-02-16 18:26:38 -0800

    02162019 forms

--- react-redux/my-app/src/App.js
+++ react-redux/my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Ninjas from "./ninjas";
+import AddNinja from "./addNinja";
 
 class App extends Component {
   state = {
@@ -15,6 +16,7 @@ class App extends Component {
         <h1>Test React App</h1>
         <p> Welcome </p>
         <Ninjas ninjas= {this.state.ninjas} />
+        <AddNinja />
       </div>
     );
   }
