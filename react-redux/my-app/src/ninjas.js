71835bf4b00e274775b04a94346d315d77bd1eb1
mjbenefiel <michaeljbenefiel@gmail.com>
2019-02-16 18:05:55 -0800

    02162019 ternary

--- react-redux/my-app/src/ninjas.js
+++ react-redux/my-app/src/ninjas.js
@@ -1,9 +1,8 @@
 import React from "react";
 // ninjas is destructured
-const Ninjas = ({ninjas}) => {
-  
-    // console.log(this.props);
-    const ninjaList = ninjas.map(ninja =>{
+const Ninjas = ({ ninjas }) => {
+  // console.log(this.props);
+  /*   const ninjaList = ninjas.map(ninja =>{
       if (ninja.age > 25) {
       return (
      <div className="ninja" key={ninja.id}>
@@ -13,15 +12,20 @@ const Ninjas = ({ninjas}) => {
       </div>
       )
       } else {
-        return null
+        return
       }
-    })
-    return (
-      <div className="ninja-list">
-      { ninjaList }
+    }) */
+  const ninjaList = ninjas.map(ninja => {
+    // condition ? () : ()
+    return ninja.age > 25 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name} </div>
+        <div>Age: {ninja.age} </div>
+        <div>Belt: {ninja.belt} </div>
       </div>
-    );
-
-}
+    ) : null;
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninjas;
