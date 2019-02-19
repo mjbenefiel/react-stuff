2ff2389b5b87146c49eb736c38d7bb68eab68463
mjbenefiel <michaeljbenefiel@gmail.com>
2019-02-18 20:48:24 -0800

    02182019 app delete function

--- react-redux/todoapp/src/App.js
+++ react-redux/todoapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
 
 class App extends Component {
   state = {
@@ -7,10 +8,19 @@ class App extends Component {
       {id: 2, content: 'play mario kart'}
     ]
   }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    })
+  }
   render() {
     return (
-      <div className="App">
-        
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
