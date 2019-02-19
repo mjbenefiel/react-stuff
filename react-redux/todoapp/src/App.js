2f20b80c9696c0e38ba0336c20d046b8e2b0f684
mjbenefiel <michaeljbenefiel@gmail.com>
2019-02-18 20:28:52 -0800

    02182019 new app

---
+++ react-redux/todoapp/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
