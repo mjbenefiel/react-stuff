f8e559e6f2922dbb2890e8f5f3055e60254dbaf7
mjbenefiel <michaeljbenefiel@gmail.com>
2019-02-19 20:19:04 -0800

    02192019 React Router

---
+++ react-redux/routing-demo/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+        <h4 className="center">About</h4>
+        <p>LCulpa quis non non excepteur velit in mollit consectetur consequat. Labore nostrud minim proident eu. Magna ea exercitation elit aute labore amet sunt est consectetur aliqua non nisi. Ea aute eiusmod incididunt sit. Exercitation id elit velit ipsum.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ react-redux/routing-demo/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+        <h4 className="center">Home</h4>
+       <p>LCulpa quis non non excepteur velit in mollit consectetur consequat. Labore nostrud minim proident eu. Magna ea exercitation elit aute labore amet sunt est consectetur aliqua non nisi. Ea aute eiusmod incididunt sit. Exercitation id elit velit ipsum.</p>
+        </div>
+    )
+}
+
+export default Home;
