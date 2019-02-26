d75de4703da6c3cdd0d87f09a7d49ddb026d1cd1
mjbenefiel <michaeljbenefiel@gmail.com>
2019-02-25 18:01:34 -0800

    02252019 add_todo, add_post

--- react-redux/redux-starter/redux.js
+++ react-redux/redux-starter/redux.js
@@ -7,12 +7,34 @@ const initState = {
 }
 
 function myreducer(state = initState, action) {
-    console.log(action, state);
+    // console.log(action, state);
+    if (action.type == 'ADD_TODO') {
+        return {
+            ...state, 
+            todos: [...state.todos, action.todo]
+        }
+    }
+
+     if (action.type == 'ADD_POST') {
+        return {
+            ...state, 
+            todos: [...state.posts, action.post]
+        }
+    }
 }
 
 const store = createStore(myreducer);
 
-const todoAction = { type: 'ADD_TODO', todo: 'buy milk' };
+store.subscribe((() => {
+    // fires every time state is changed
+    console.log('state updated')
+    console.log(store.getState())
+})
 
+// const todoAction = { type: 'ADD_TODO', todo: 'buy milk' };
 // dispatching action to reducer
-store.dispatch(todoAction)
+// store.dispatch(todoAction)
+
+store.dispatch({type: 'ADD_TODO', todo: 'buy milk'})
+store.dispatch({type: 'ADD_TODO', todo: 'exercise'})
+store.dispatch({type: 'ADD_POST', post: 'dine food'})
