ed5411ac4a3bb0b062385523069c0d4268f2ddf7
mjbenefiel <michaeljbenefiel@gmail.com>
2019-02-25 17:43:07 -0800

    02252019 dispatching actions

--- react-redux/redux-starter/redux.js
+++ react-redux/redux-starter/redux.js
@@ -7,7 +7,12 @@ const initState = {
 }
 
 function myreducer(state = initState, action) {
-
+    console.log(action, state);
 }
 
 const store = createStore(myreducer);
+
+const todoAction = { type: 'ADD_TODO', todo: 'buy milk' };
+
+// dispatching action to reducer
+store.dispatch(todoAction)
