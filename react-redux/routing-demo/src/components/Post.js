6cd84216168c2dcbe68fc06bfec59c549699606f
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-04 20:06:12 -0800

    03042019 action creators

--- react-redux/routing-demo/src/components/Post.js
+++ react-redux/routing-demo/src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import axios from "axios";
 import { connect } from "react-redux";
+import { deletePost } from "../actions/postActions"
 
 class Post extends Component {
   handleClick = () => {
@@ -38,7 +39,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = dispatch => {
   return {
     deletePost: id => {
-      dispatch({ type: "DELETE_POST", id: id });
+      dispatch(deletePost(id));
     }
   };
 };
