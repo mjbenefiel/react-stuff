d2bdd4167b85e9249249dba34a8c0ab2a1fe8c67
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-04 20:00:37 -0800

    03042019 delete post, map dispatch to post

--- react-redux/routing-demo/src/components/Post.js
+++ react-redux/routing-demo/src/components/Post.js
@@ -1,27 +1,30 @@
 import React, { Component } from "react";
-import axios from "axios";
-import { connect } from "react-redux"
+// import axios from "axios";
+import { connect } from "react-redux";
 
 class Post extends Component {
- 
-
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/')
+  }
   render() {
+    console.log(this.props)
 
     const post = this.props.post ? (
-        <div className="post">
+      <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
         </div>
-    ) : (
-        <div className="center">loading post...</div>
-        
-    )
-
-    return (
-      <div className="container">
-        { post }
       </div>
-    )
+    ) : (
+      <div className="center">loading post...</div>
+    );
+
+    return <div className="container">{post}</div>;
   }
 }
 
@@ -29,7 +32,18 @@ const mapStateToProps = (state, ownProps) => {
   let id = ownProps.match.params.post_id;
   return {
     post: state.posts.find(post => post.id === id)
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(Post)
+const mapDispatchToProps = dispatch => {
+  return {
+    deletePost: id => {
+      dispatch({ type: "DELETE_POST", id: id });
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Post);
