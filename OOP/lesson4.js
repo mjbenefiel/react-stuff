2588e82a9049d7e1d04563f8b26954ab7027f937
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-04 21:03:44 -0800

    03042019 method chaining

--- OOP/lesson4.js
+++ OOP/lesson4.js
@@ -2,20 +2,33 @@ class User {
   constructor(email, name) {
       this.email = email;
       this.name = name;
+      this.score = 0;
   }
 login(){
     console.log(this.email, "just logged in");
+    return this //returning object
 }
 logout(){
     console.log(this.email, "just logged out");
+    return this //returning object
+}
+updateScore() {
+    this.score++;
+    console.log(this.email, 'score is now', this.score);
+    return this //returning object
 }
 }
 
 var userOne = new User('sample@sample.com', 'Mike')
 var userTwo = new User('sample1@sample.com', 'Mike1')
 
-userOne.login()
-userTwo.logout()
+// call the methods || method calls
+userOne.login();
+userTwo.logout();
+userOne.updateScore();
+userOne.updateScore();
+console.log('----------------------')
+userOne.login().updateScore().updateScore().logout()
 
 console.log(userOne)
 console.log(userTwo)
