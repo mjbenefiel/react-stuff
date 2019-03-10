f78d29cebbc484cb6673cec81b0a5deddd87d4c6
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-10 12:15:19 -0700

    03102019 react

--- Webpack/src/app.js
+++ Webpack/src/app.js
@@ -1,13 +1,27 @@
-import "./styles/styles.scss";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+var customElement = React.createElement("h1", null, "Hello React")
+var anotherCustomElement = <h1>
+    Hello World
+</h1>;
+
+ReactDOM.render(anotherCustomElement, document.getElementById("app"))
+
+
+
+
+/*import "./styles/styles.scss";
 
 // import { add, subtract, multiply, divide } from "./script.js"
-/* import * as functions from "./script.js";
+import * as functions from "./script.js";
 import printName from "./script.js"
 
 console.log(functions.add(5, 25))
 
 console.log(functions.divide(35, 25))
-printName("mike")*/
+printName("mike")
 
 const arrowFunction = (name) => name;
 console.log(arrowFunction("Hey"));
@@ -21,4 +35,4 @@ const advancedUser = {
     ...user,
     age: 34
 };
-console.log(advancedUser);
+console.log(advancedUser); */
