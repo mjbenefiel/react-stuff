1ad951222f451bc421861fe07c0a9dba66dd84ac
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-12 14:09:58 -0700

    testing w/ object

--- Jest/functions.js
+++ Jest/functions.js
@@ -1,5 +1,12 @@
 const functions = {
-    add: (num1, num2) => num1 + num2 
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = {firstName: 'Mike'}
+        user['lastName'] = 'Benefiel';
+        return user
+    }
 }
 
 module.exports = functions
--- Jest/functions.test.js
+++ Jest/functions.test.js
@@ -7,3 +7,15 @@ test('Adds 2 + 2 to equal 4', () => {
 test('Adds 2 + 2 to not equal 4', () => {
     expect (functions.add(2, 2)).not.toBe(5)
 })
+
+test('Should be null', () => {
+    expect (functions.isNull()).toBeNull()
+})
+
+test('Should be falsy', () => {
+    expect (functions.checkValue(null)).toBeFalsy()
+})
+
+test('user should be Mike Benefiel object', () => {
+    expect (functions.createUser()).toEqual({firstName: 'Mike', lastName: 'Benefiel'})
+})
