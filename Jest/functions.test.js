85a8e154c030dc51348c55dc0227175b9e2554e9
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-12 14:00:42 -0700

    passed tests

--- Jest/functions.test.js
+++ Jest/functions.test.js
@@ -1,5 +1,9 @@
 const functions = require('./functions');
 
-test('Adds 2 + 2 to requal 4', () => {
+test('Adds 2 + 2 to equal 4', () => {
     expect (functions.add(2, 2)).toBe(4)
+})
+
+test('Adds 2 + 2 to not equal 4', () => {
+    expect (functions.add(2, 2)).not.toBe(5)
 })
