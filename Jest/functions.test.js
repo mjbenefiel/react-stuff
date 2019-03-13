b2479ed961f85d5d6e89360a04cb344d01d77741
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-13 13:16:52 -0700

    03132019 async await

--- Jest/functions.test.js
+++ Jest/functions.test.js
@@ -1,41 +1,53 @@
-const functions = require('./functions');
+const functions = require("./functions");
 
-test('Adds 2 + 2 to equal 4', () => {
-    expect (functions.add(2, 2)).toBe(4)
-})
+test("Adds 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
 
-test('Adds 2 + 2 to not equal 4', () => {
-    expect (functions.add(2, 2)).not.toBe(5)
-})
+test("Adds 2 + 2 to not equal 4", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
 
-test('Should be null', () => {
-    expect (functions.isNull()).toBeNull()
-})
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
 
-test('Should be falsy', () => {
-    expect (functions.checkValue(null)).toBeFalsy()
-})
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
 
-test('user should be Mike Benefiel object', () => {
-    expect (functions.createUser()).toEqual({firstName: 'Mike', lastName: 'Benefiel'})
-})
+test("user should be Mike Benefiel object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Mike",
+    lastName: "Benefiel"
+  });
+});
 
-test ('should be under 1600', () => {
-    const load1 = 800;
-    const load2 = 700;
-    expect(load1 + load2).toBeLessThan(1600)
-})
+test("should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
 
-test ('Admin should be in user name', () => {
-    usernames = ['john', 'karen', 'admin'];
-    expect (usernames).toContain('admin')
-})
+test("Admin should be in user name", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
+});
 
 // working with async data
-test ('User fetched name should be Leanne Graham', () => {
-   expect.assertions(1);
-   return functions.fetchUser()
-   .then(data => {
-       expect(data.name).toEqual('Leanne Graham')
-   })
-})
+
+// Promise
+/*test("User fetched name should be Leanne Graham", () => {
+  expect.assertions(1);
+  return functions.fetchUser().then(data => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
+}); */
+
+// Async/Await
+
+test("User fetched name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
+});
