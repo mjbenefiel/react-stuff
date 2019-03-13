246741a047f2186c99c826899c7ca3eccaf05d19
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-13 13:13:25 -0700

    03132019 async testing

--- Jest/functions.test.js
+++ Jest/functions.test.js
@@ -30,3 +30,12 @@ test ('Admin should be in user name', () => {
     usernames = ['john', 'karen', 'admin'];
     expect (usernames).toContain('admin')
 })
+
+// working with async data
+test ('User fetched name should be Leanne Graham', () => {
+   expect.assertions(1);
+   return functions.fetchUser()
+   .then(data => {
+       expect(data.name).toEqual('Leanne Graham')
+   })
+})
