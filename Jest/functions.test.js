c458ee436f0f760bf62fc1003a632c99a2e5dcb4
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-13 13:05:32 -0700

    03132019 array testing

--- Jest/functions.test.js
+++ Jest/functions.test.js
@@ -19,3 +19,14 @@ test('Should be falsy', () => {
 test('user should be Mike Benefiel object', () => {
     expect (functions.createUser()).toEqual({firstName: 'Mike', lastName: 'Benefiel'})
 })
+
+test ('should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600)
+})
+
+test ('Admin should be in user name', () => {
+    usernames = ['john', 'karen', 'admin'];
+    expect (usernames).toContain('admin')
+})
