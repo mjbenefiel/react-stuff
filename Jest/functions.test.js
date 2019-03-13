5c0e433860be423b73169ffb872944fdf5e04e04
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-13 14:33:02 -0700

    03132019 lifecycle methods

--- Jest/functions.test.js
+++ Jest/functions.test.js
@@ -1,5 +1,27 @@
 const functions = require("./functions");
 
+/*beforeEach(() => initDatabase())
+afterEach(() => closeDatabase())
+
+const initDatabase = () => console.log('Database Initialized')
+const closeDatabase = () => console.log('Database Closed') */
+const nameCheck = () => console.log('Checking name')
+
+describe('Checking Names', () => {
+  beforeEach(() => nameCheck())
+
+  test('User is Jeff', () => {
+    const user = 'Jeff';
+    expect(user).toBe('Jeff')
+  })
+
+  test('User is Karen', () => {
+    const user = 'Karen';
+    expect(user).toBe('Karen')
+  })
+})
+
+
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
