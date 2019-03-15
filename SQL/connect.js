88e591099cc088b8bdf800f1f3cff05a16adb928
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-15 11:06:55 -0700

    adding table

--- SQL/connect.js
+++ SQL/connect.js
@@ -1,23 +1,32 @@
 let mysql = require('mysql');
-
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'mjbenefiel',
-    password: 'dur@cell15',
-    database: 'todoapps'
-})
-
-connection.connect(function(err){
-    if (err) {
-        return console.error('error' + err.message)
-    }
-
-    console.log('Connected!')
+  host: 'localhost',
+  user: 'mjbenefiel',
+  password: '',
+  database: 'todoapps'
 });
-
-connection.end(function(err) {
+ 
+// connect to the MySQL server
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+ 
+  let createTodos = `create table if not exists todos(
+                          id int primary key auto_increment,
+                          title varchar(255)not null,
+                          completed tinyint(1) not null default 0
+                      )`;
+ 
+  connection.query(createTodos, function(err, results, fields) {
     if (err) {
-        return console.log('error:' + err.message);
+      console.log(err.message);
     }
-    console.log('Close the database')
-})
+  });
+ 
+  connection.end(function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+  });
+});
