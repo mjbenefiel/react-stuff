356a66c31a78d0a61dc167c442e66434ab88e7dc
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-15 10:50:10 -0700

    03152019 node/sql connection

--- SQL/connect.js
+++ SQL/connect.js
@@ -1,1 +1,23 @@
-CREATE DATABASE todoapp;
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'mjbenefiel',
+    password: 'dur@cell15',
+    database: 'todoapps'
+})
+
+connection.connect(function(err){
+    if (err) {
+        return console.error('error' + err.message)
+    }
+
+    console.log('Connected!')
+});
+
+connection.end(function(err) {
+    if (err) {
+        return console.log('error:' + err.message);
+    }
+    console.log('Close the database')
+})
