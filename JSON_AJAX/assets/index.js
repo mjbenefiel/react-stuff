a09b196c347845a527f517dfe55e00d8e1b61515
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-17 17:06:14 -0700

    03172019 ajax call

---
+++ JSON_AJAX/assets/index.js
@@ -0,0 +1,12 @@
+var ourRequest = new XMLHttpRequest();
+ourRequest.open(
+  "GET",
+  "https://learnwebcode.github.io/json-example/animals-1.json"
+);
+ourRequest.onload = () => {
+  // console.log(ourRequest.responseText);
+  var ourData = JSON.parse(ourRequest.responseText);
+  console.log(ourData[0])
+};
+
+ourRequest.send();
--- JSON_AJAX/readme.md
+++ JSON_AJAX/readme.md
@@ -3,3 +3,7 @@ JSON | AJAX
 - JSON
     - JavaScript Object Notation
 
+- AJAX
+    - Asynchronous JavaScript and XML
+    - Process of sending/receiving data on the fly without page reload
+
