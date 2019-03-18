f04261f02bc0491297bb6daf6b9897ab74230e07
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-17 17:39:59 -0700

    03172019 GET cont'd

--- JSON_AJAX/assets/index.js
+++ JSON_AJAX/assets/index.js
@@ -7,7 +7,9 @@ btn.addEventListener("click", () => {
   var ourRequest = new XMLHttpRequest();
   ourRequest.open(
     "GET",
-    "https://learnwebcode.github.io/json-example/animals-"+ pageCounter +".json"
+    "https://learnwebcode.github.io/json-example/animals-" +
+      pageCounter +
+      ".json"
   );
   ourRequest.onload = () => {
     // console.log(ourRequest.responseText);
@@ -17,16 +19,39 @@ btn.addEventListener("click", () => {
   };
 
   ourRequest.send();
-  pageCounter++
+  pageCounter++;
   if (pageCounter > 3) {
-    btn.classList.add("hide-me")
+    btn.classList.add("hide-me");
   }
 });
 
-const renderHTML = (data) => {
-  var htmlString = ""
+const renderHTML = data => {
+  var htmlString = "";
   for (i = 0; i < data.length; i++) {
-    htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>"
+    htmlString +=
+      "<p>" +
+      data[i].name +
+      " is a " +
+      data[i].species +
+      ". that likes to eat ";
+    for (j = 0; j < data[i].foods.likes.length; j++) {
+      if (j === 0) {
+        htmlString += data[i].foods.likes[j];
+      } else {
+        htmlString += " and " + data[i].foods.likes[j];
+      }
+    }
+
+    htmlString += " and dislikes ";
+
+    for (j = 0; j < data[i].foods.dislikes.length; j++) {
+      if (j === 0) {
+        htmlString += data[i].foods.dislikes[j];
+      } else {
+        htmlString += " and " + data[i].foods.dislikes[j];
+      }
+    }
+    htmlString += " .</p>";
   }
-  animalContainer.insertAdjacentHTML('beforeend', htmlString)
+  animalContainer.insertAdjacentHTML("beforeend", htmlString);
 };
