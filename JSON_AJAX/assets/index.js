11fd0d185451b10b9fea92e61405259e8d8612a7
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-17 17:31:22 -0700

    03172019 GET

--- JSON_AJAX/assets/index.js
+++ JSON_AJAX/assets/index.js
@@ -1,19 +1,32 @@
+var pageCounter = 1;
+
+var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
   var ourRequest = new XMLHttpRequest();
-ourRequest.open(
-  "GET",
-  "https://learnwebcode.github.io/json-example/animals-1.json"
-);
-ourRequest.onload = () => {
-  // console.log(ourRequest.responseText);
-  var ourData = JSON.parse(ourRequest.responseText);
-  console.log(ourData[0]);
-};
+  ourRequest.open(
+    "GET",
+    "https://learnwebcode.github.io/json-example/animals-"+ pageCounter +".json"
+  );
+  ourRequest.onload = () => {
+    // console.log(ourRequest.responseText);
+    var ourData = JSON.parse(ourRequest.responseText);
+    // console.log(ourData[0]);
+    renderHTML(ourData);
+  };
 
-ourRequest.send();
-
+  ourRequest.send();
+  pageCounter++
+  if (pageCounter > 3) {
+    btn.classList.add("hide-me")
+  }
 });
 
-
+const renderHTML = (data) => {
+  var htmlString = ""
+  for (i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>"
+  }
+  animalContainer.insertAdjacentHTML('beforeend', htmlString)
+};
