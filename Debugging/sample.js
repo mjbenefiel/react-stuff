6ff679a53518e9765a108fd792ea001e38a48b3d
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-18 18:14:45 -0700

    03182019 prep

---
+++ Debugging/sample.js
@@ -0,0 +1,24 @@
+/*var myName = "Mike"
+
+if (typeof myName !== 'undefined') {
+    console.log("The variable is defined " + myName)
+} else {
+    console.log("The variable is not defined")
+}*/
+/* var arr1 = ['a','b','c','d','e','f'];
+arr1.length = 0;
+console.log(arr1); //output = [] */
+
+
+
+/*function isPalindrome (word){
+  word = word.toLowerCase();
+  return word === word.split("").reverse().join("");
+}
+
+if (isPalindrome("dog god") === true) {
+    console.log("It's a palindrome")
+} else {
+    console.log ("It's not a palindrome")
+} */
+
