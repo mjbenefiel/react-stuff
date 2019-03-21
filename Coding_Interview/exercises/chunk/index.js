74028fdfbb3a4aa32b1e3f8c4481f068d676a95f
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-21 12:43:54 -0700

    03212019 chunking

--- Coding_Interview/exercises/chunk/index.js
+++ Coding_Interview/exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+/*function chunk(array, size) {
+    let chunked = [];
+    for (let element of array) {
+        const last = chunked[chunked.length - 1];
+        if (!last || last.length === size) {
+            chunked.push([element])
+        } else {
+            last.push(element)
+        }
+    }
+
+    return chunked;
+}
+console.log(chunk([1,2,3,4,5], 2)) */
+
+/*function chunk(array, size) {
+    let chunked = [];
+    for (var i = 0; i <=array.length; i++) {
+        const last = chunked[chunked.length - 1];
+        if (!last || last.length === size){
+            chunked.push([i])
+        } else {
+            last.push(i)
+        }
+    }
+    return chunked;
+} 
+console.log(chunk([1,2,3,4,5], 2))
+*/
+
+function chunk(array, size) {
+    
+}
+
 
 module.exports = chunk;
