7d218141ffb5bd3f2ccafedd86313da14882c471
mjbenefiel <michaeljbenefiel@gmail.com>
2019-03-21 17:46:57 -0700

    03212019 anagram

--- Coding_Interview/exercises/anagrams/index.js
+++ Coding_Interview/exercises/anagrams/index.js
@@ -8,6 +8,78 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//  RegEx to remove spaces and caps -- const word = "HI THERE!" word.replace(/[^w]/g, "").toLowerCase();
+
+function anagrams(stringA, stringB) {
+  if (cleanString(stringA) === cleanString(stringB)) {
+    console.log("anagram");
+  } else {
+    console.log("no anagram");
+  }
+}
+
+anagrams("Hello there!", "There. Hello!");
+
+// helper function to clean up strings
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+}
 
 module.exports = anagrams;
+
+/*function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+
+    for (var i = 0; i < aCharMap.length; i++) {
+        let char = aCharMap[i]
+        if (aCharMap[char] !== bCharMap[char]) {
+            return false
+        }
+    }
+
+    return true;
+}
+
+function buildCharMap(str){
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
+
+/*function buildCharMap(str) {
+    const charMap = {};
+    let char = str[i];
+
+    for (var i = 0; i <= str.replace(/[^\w]/g, '').toLowerCase()) {
+        let char = str[i];
+        charMap[char] = charMap[char] + 1 || 1;
+
+    }
+
+    return charMap;
+} */
+
+/*function buildCharMap(str){
+    const charMap = {};
+
+    for (let char of str.replace([^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}*/
