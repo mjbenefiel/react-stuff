3c2f8511e9a324c78b0877eff0028837891ff268
mjbenefiel <michaeljbenefiel@gmail.com>
2019-04-07 10:46:02 -0700

    capitalize alternate

--- Coding_Interview/exercises/capitalize/index.js
+++ Coding_Interview/exercises/capitalize/index.js
@@ -8,6 +8,23 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+console.log(capitalize("hello"));
+
+module.exports = capitalize;
+
+/*function capitalize(str) {
   const words = [];
   for (let word of str.split(' ')) {
       words.push(word[0].toUpperCase() + word.slice(1))
@@ -15,6 +32,4 @@ function capitalize(str) {
   return words.join(' ')
 }
 
-console.log(capitalize("hello"))
-
-module.exports = capitalize;
+console.log(capitalize("hello")) */
