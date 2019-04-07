dba68cae8e2d4c7d620147bb99f97eb75e1ac219
mjbenefiel <michaeljbenefiel@gmail.com>
2019-04-07 10:25:47 -0700

    04072019 capitalize

--- Coding_Interview/exercises/capitalize/index.js
+++ Coding_Interview/exercises/capitalize/index.js
@@ -8,12 +8,11 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    let words = [];
-    for (let z of str.split(' ')) {
-        words.push(word[0].toUpperCase() + word.slice(1))
-    }
-
-    return words.join(' ');
+  const words = [];
+  for (let word of str.split(' ')) {
+      words.push(word[0].toUpperCase() + word.slice(1))
+  }
+  return words.join(' ')
 }
 
 console.log(capitalize("hello"))
