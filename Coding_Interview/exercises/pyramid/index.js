c7489ef43c8adc70771eb2ec6463bf32e9286398
mjbenefiel <michaeljbenefiel@gmail.com>
2019-04-15 11:40:52 -0700

    pyramid #2

--- Coding_Interview/exercises/pyramid/index.js
+++ Coding_Interview/exercises/pyramid/index.js
@@ -14,7 +14,29 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  pyramid(n, row, level + add);
+}
+console.log(pyramid(5));
+
+module.exports = pyramid;
+/*function pyramid(n) {
     const midpoint = Math.floor((2 * n - 1) / 2);
     for (let row = 0; row < n; row++){
         let level = '';
@@ -29,6 +51,4 @@ function pyramid(n) {
         console.log(level)
     }
 }
-console.log (pyramid(5));
-
-module.exports = pyramid;
+console.log (pyramid(5)); */
